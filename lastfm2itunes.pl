811f5ddc89bf934ba97de09068e2852ec1b9c59b
woobind <woobind@gmail.com>
2012-03-05 15:05:51 +0000

    First upload

---
+++ lastfm2itunes.pl
@@ -0,0 +1,105 @@
+#!/usr/bin/perl
+
+use strict;
+use XML::DOM;
+use Class::Struct;
+use Encode;
+use POSIX qw(strftime);
+
+use Win32::OLE 'CP_UTF8';
+$Win32::OLE::CP = CP_UTF8;
+my $parser = new XML::DOM::Parser;
+
+# User static settings
+my $username = '';		# <= Put your username here
+my $verbose = 0;		# <= set 1 to enable verbose output
+
+# Script static variables
+my $chart_list_url   = 'http://ws.audioscrobbler.com/2.0/user/<USER>/weeklychartlist.xml';
+my $weekly_chart_url = 'http://ws.audioscrobbler.com/2.0/user/<USER>/weeklytrackchart.xml?from=<FROM>&to=<TO>';
+my $version = 'v0.1 (05.03.2012)';
+
+# Script dynamic variables
+my %lastfm_track_playcount = ();
+my %lastfm_track_playlast = ();
+
+print "iTunes Library Updater " . $version . "\n";
+print "Copyright (c) 2012 Roman Gemini (roman_gemini\@ukr.net)\n\n";
+if($username eq '') {
+	print "Set variable '\$username' first!\n";
+	print "Press <ENTER> to exit...";
+	<>;
+	exit;
+}
+
+print "Trying to connect to iTunes scripting interface...\n";
+my $iTunes = Win32::OLE->GetActiveObject('iTunes.Application');
+unless ($iTunes) { $iTunes = new Win32::OLE('iTunes.Application'); }
+unless ($iTunes) { die("Couldn't connect to iTunes!"); }
+
+print "Fetching week charts...\n";
+my $url = $chart_list_url;
+$url =~ s/<USER>/$username/g;
+my $charts_data = $parser->parsefile($url) || die("Can't fetch charts list XML!");
+my $root_charts_data = $charts_data->getElementsByTagName("weeklychartlist") || die("Can't parse charts list XML!");
+my $weeks_charts_data = $root_charts_data->item(0)->getElementsByTagName("chart") || die("Can't parse charts list XML!");
+for my $i (0 .. $weeks_charts_data->getLength-1) {
+	my $item = $weeks_charts_data->item($i);
+	my $from = $item->getAttribute("from");
+	my $to = $item->getAttribute("to");
+
+	my $w_url = $weekly_chart_url;
+	$w_url =~ s/<USER>/$username/g;
+	$w_url =~ s/<FROM>/$from/g;
+	$w_url =~ s/<TO>/$to/g;
+	print sprintf("Fetching chart for week %3d of %3d (%s - %s)\n", $i + 1, $weeks_charts_data->getLength, strftime("%Y-%m-%d", localtime($from)), strftime("%Y-%m-%d", localtime($to)));
+
+	my $week_data;
+	eval { $week_data = $parser->parsefile($w_url) };
+	next if(!$week_data); 
+
+	my $week_root = $week_data->getElementsByTagName("weeklytrackchart");
+	my $week_tracks = $week_root->item(0)->getElementsByTagName("track");
+
+	for my $j (0 .. $week_tracks->getLength-1) {
+		my $track = $week_tracks->item($j);
+		my $tag_title = $track->getElementsByTagName("name")->item(0)->getFirstChild->getData();
+		my $tag_artist = $track->getElementsByTagName("artist")->item(0)->getFirstChild->getData();
+		my $tag_play_count = $track->getElementsByTagName("playcount")->item(0)->getFirstChild->getData();
+		print sprintf("\tTrack \"%s\" played %d time(s)\n", _866("$tag_artist - $tag_title"), $tag_play_count) if($verbose);
+
+
+		$lastfm_track_playcount{lc($tag_artist)}{lc($tag_title)} += $tag_play_count;
+		$lastfm_track_playlast{lc($tag_artist)}{lc($tag_title)} = strftime("%Y-%m-%d %H:%M:%S", localtime($from));
+	}
+}
+
+print "\nScanning iTunes library...\n";
+for my $k_artist (sort keys %lastfm_track_playcount) {
+	for my $k_title (sort keys %{$lastfm_track_playcount{$k_artist}}) {
+		print sprintf("\tSearching library for \"%s\"\n", _866("$k_artist - $k_title")) if($verbose);
+		my $tmp_count = $lastfm_track_playcount{$k_artist}{$k_title};
+		my $tmp_last = $lastfm_track_playlast{$k_artist}{$k_title};
+		my $iTunes_DATA = $iTunes->LibraryPlaylist;
+		my $iTunes_LIB = $iTunes_DATA->Search("$k_artist $k_title", 1);
+		if($iTunes_LIB) {
+			for my $t (1 .. $iTunes_LIB->Count) {
+				my $trk = $iTunes_LIB->Item($t);
+				if(($trk->kind() == 1) and (lc($trk->artist()) eq $k_artist) and (lc($trk->name()) eq $k_title) and ($trk->playCount()<$tmp_count)) {
+					my $trID = $trk->trackID();
+					print sprintf("\t\tUpdating \"%s\": [playCount] = '%d' [playDate] = '%s' (trackID = '%d')\n", _866("$k_artist - $k_title"), $tmp_count, $tmp_last, $trID);
+					$trk->{PlayedCount} = $tmp_count;
+					$trk->{PlayedDate} = $tmp_last;
+				}
+			}
+		}
+	}
+}
+
+undef $charts_data;
+undef $parser;
+undef $iTunes;
+print "Done! Press <ENTER> to continue...\n";
+<>;
+
+sub _866 { return encode("cp866", shift); }
